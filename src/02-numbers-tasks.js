18a09020cc8521f0cf4e418d3e8bf9c02073f01e
drDramen <vvpalchin@gmail.com>
2022-12-03 21:56:15 +0200

    feat: implement "isPrime" function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -207,8 +207,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 3) return n > 1;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+
+  return true;
 }
 
 /**
